daea02b5b9935db48bafd18a88441426a293e89e
Drake Wilcox <drakewilcox@gmail.com>
2020-02-07 11:35:47 -0800

    add test for the returned age if user was on Mercury.

--- __tests__/super-galactic-test.js
+++ __tests__/super-galactic-test.js
@@ -6,8 +6,12 @@ describe ('Calculator', () => {
     calculator = new Calculator();
     calculator.addAge(30);
   });
-  test('should return the inputted age of the user', () =>{
+  test('should return the inputted age of the user', () => {
     expect(calculator.age).toEqual(30);
+  });
+  test('should return the age of user as age on Mercury', () => {
+    expect(calculator.mercuryAge).toEqual()
+    
   })
 
-})
+});
--- src/super-galactic.js
+++ src/super-galactic.js
@@ -1,9 +1,14 @@
 export class Calculator {
   contructor() {
     this.age = "";
+    this.mercuryAge = "";
+    this.venusAge = "";
+    this.marsAge = "";
+    this.jupiterAge = "";
+    this.sex = "";
+    this.lifeExp = "";
   }
   addAge(age) {
     this.age = age;
-  
   }
 }
